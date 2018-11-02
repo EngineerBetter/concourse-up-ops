b0047536a484079821cfda9a79e58f6b6e24047f
Irbe Krumina <irbe.krumina@engineerbetter.com>
2018-11-02 18:24:52 +0000

    enriching GCP terraform configuration
    Signed-off-by: Panagiotis Xynos <panos@engineerbetter.com>

--- director/gcp/infrastructure.tf
+++ director/gcp/infrastructure.tf
@@ -1,26 +1,65 @@
+variable "zone" {
+  type = "string"
+	default = "<% .Zone %>"
+}
+variable "tags" {
+  type = "string"
+	default = "<% .Tags %>"
+}
+variable "project" {
+  type = "string"
+	default = "<% .Project %>"
+}
+variable "gcpcredentialsjson" {
+  type = "string"
+	default = "<% .GCPCredentialsJSON %>"
+}
+variable "externalip" {
+  type = "string"
+	default = "<% .ExternalIP %>"
+}
+
+variable "deployment" {
+  type = "string"
+	default = "<% .Deployment %>"
+}
+variable "region" {
+  type = "string"
+	default = "<% .Region %>"
+}
+
 provider "google" {
-  project = "concourse-up"
-  region = "us-east1"
+    credentials = "<% .GCPCredentialsJSON %>"
+    project = "<% .Project %>"
+    region = "us-east1"
+}
+
+
+terraform {
+	backend "gcs" {
+		bucket = "<% .ConfigBucket %>"
+		region = "<% .Region %>"
+	}
 }
 
 // route for nat
 resource "google_compute_route" "nat" {
-  name                   = "nat-route"
+  name                   = "${var.deployment}-nat-route"
   dest_range             = "0.0.0.0/0"
   network                = "${google_compute_network.bosh.name}"
   next_hop_instance      = "${google_compute_instance.nat-instance.name}"
-  next_hop_instance_zone = "us-east1-c"
+  next_hop_instance_zone = "${var.zone}"
   priority               = 800
   tags                   = ["no-ip"]
-  project                = "concourse-up"
+  project                = "${var.project}"
 }
 
 // nat
 resource "google_compute_instance" "nat-instance" {
-  name         = "nat-instance"
+  name         = "${var.deployment}-nat-instance"
   machine_type = "n1-standard-1"
-  zone         = "us-east1-c"
-  project      = "concourse-up"
+  zone         = "${var.zone}"
+  project      = "${var.project}"
 
   tags = ["nat", "internal"]
 
@@ -32,7 +71,7 @@ resource "google_compute_instance" "nat-instance" {
 
   network_interface {
     subnetwork = "${google_compute_subnetwork.private.name}"
-    subnetwork_project = "concourse-up"
+    subnetwork_project = "${var.project}"
     access_config {
       // Ephemeral IP
     }
@@ -47,25 +86,25 @@ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 EOT
 }
 resource "google_compute_network" "bosh" {
-  name                    = "bosh-network"
-  project                 = "concourse-up"
+  name                    = "${var.deployment}-bosh-network"
+  project                 = "${var.project}"
   auto_create_subnetworks = "false"
 }
 
 resource "google_compute_subnetwork" "public" {
-  name          = "bosh-us-east1-subnet-public"
+  name          = "${var.deployment}-bosh-us-east1-subnet-public"
   ip_cidr_range = "10.0.0.0/24"
   network       = "${google_compute_network.bosh.self_link}"
-  project       = "concourse-up"
+  project       = "${var.project}"
 }
 resource "google_compute_subnetwork" "private" {
-  name          = "bosh-us-east1-subnet-private"
+  name          = "${var.deployment}-bosh-us-east1-subnet-private"
   ip_cidr_range = "10.0.1.0/24"
   network       = "${google_compute_network.bosh.self_link}"
-  project       = "concourse-up"
+  project       = "${var.project}"
 }
 resource "google_service_account" "bosh" {
-  account_id   = "boshaccount"
+  account_id   = "${var.deployment}-boshaccount"
   display_name = "bosh"
 }
 resource "google_service_account_key" "bosh" {
@@ -74,36 +113,36 @@ resource "google_service_account_key" "bosh" {
 }
 
 resource "google_project_iam_member" "bosh" {
-  project = "concourse-up"
+  project = "${var.project}"
   role    = "roles/owner"
   member  = "serviceAccount:${google_service_account.bosh.email}"
 }
 resource "google_compute_address" "atc_ip" {
-  name = "atc-ip"
+  name = "${var.deployment}-atc-ip"
 }
 
 resource "google_compute_address" "director" {
-  name = "director"
+  name = "${var.deployment}-director-ip"
 }
 
-output "private_subnet_id" {
-    value = "${google_compute_subnetwork.private.name}"
+output "network" {
+value = ""
 }
 
-output "public_subnet_id" {
-    value = "${google_compute_subnetwork.public.name}"
+output "subnetwork" {
+value = ""
 }
 
-output "vpc_id" {
-    value = "${google_compute_network.bosh.name}"
+output "internal_cidr" {
+value = ""
 }
 
-output "nat_gateway_ip" {
-    value = "${google_compute_instance.nat-instance.network_interface.0.access_config.0.nat_ip}"
+output "internal_gw" {
+value = ""
 }
 
-output "source_access_ip" {
-  value = ""
+output "external_ip" {
+value = ""
 }
 
 output "director_key_pair" {
@@ -112,52 +151,4 @@ output "director_key_pair" {
 
 output "director_public_ip" {
   value = "${google_compute_address.director.address}"
-}
-
-output "atc_public_ip" {
-  value = ""
-}
-
-output "director_security_group_id" {
-  value = ""
-}
-
-output "vms_security_group_id" {
-  value = ""
-}
-
-output "atc_security_group_id" {
-  value = ""
-}
-
-
-
-output "blobstore_bucket" {
-  value = ""
-}
-
-output "blobstore_user_access_key_id" {
-  value = ""
-}
-
-output "blobstore_user_secret_access_key" {
-  value     = ""
-  sensitive = true
-}
-
-output "bosh_user_access_key_id" {
-  value = ""
-}
-
-output "bosh_user_secret_access_key" {
-  value     = ""
-  sensitive = true
-}
-
-output "bosh_db_port" {
-  value = ""
-}
-
-output "bosh_db_address" {
-  value = ""
 }
